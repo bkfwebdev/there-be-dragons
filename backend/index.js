2187fe8801706ee7c779ffa25c91e28924f146ab
bkfwebdev <bryant.feld@gmail.com>
2020-08-30 06:27:51 -0400

    started express server

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
+const express = require("express");
 const Generation = require ("./generation");
-
 const GenerationEngine = require("./generationengine.js");
+const app = express();
 const engine = new GenerationEngine;
+const port = 3000;
 
 
 engine.start();
+app.get("/dragon/new",(req,res) => {
+    res.json({ dragon: engine.generation.newDragon() })
+});
 
-setTimeout(()=>{
-    engine.stop();
-},20000);
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+
 
 
 
